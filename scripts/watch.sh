744f94716b58b8fc06e776006682e9a52ddf1692
Clément Denis <clement@altirnao.com>
2022-11-25 11:50:27 +0100

    Fix watchify for WSL

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-./node_modules/.bin/watchify -v -o build/js/background-bundle.js src/js/background.js &
-./node_modules/.bin/watchify -v -t reactify -o build/js/client-bundle.js src/js/client.jsx &
+./node_modules/.bin/watchify --poll=100 -v -o build/js/background-bundle.js src/js/background.js &
+./node_modules/.bin/watchify --poll=100 -v -t reactify -o build/js/client-bundle.js src/js/client.jsx &
 
 
 for job in `jobs -p`
